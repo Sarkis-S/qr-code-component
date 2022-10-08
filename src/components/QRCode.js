31b05f2daa49ba9e5c25e91f643ca0f6ddbc57ae
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-10-07 21:25:44 -0700

    Create desktop component for QR code.

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,6 +1,16 @@
 
+import qrcode from '../images/image-qr-code.png';
+
 export default function QRCode () {
   return (
-    <h1>Hello World</h1>
+    <div className='qr-container'>
+      <div className='qr-items'>
+        <img className='qr-box' src={qrcode} alt='qr code'/>
+        <div className='text-container'>
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
   );
 }
