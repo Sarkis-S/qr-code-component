c80f2beeb36b746115da113c707a46e6ead80eb6
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-10-09 00:40:54 -0700

    Removefigure tag and fix styling

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -3,15 +3,13 @@ import qrcode from '../images/image-qr-code.png';
 export default function QRCode () {
   return (
     <main className='qr-container'>
-      <figure className='qr-items'>
+      <section className='qr-items'>
         <img className='qr-code' src={qrcode} alt='qr code for scanning'/>
-        <div className='text-container'>
-          <figcaption>
-            <h2>Improve your front-end skills by building projects</h2>
-          </figcaption>
-          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-        </div>
-      </figure>
+          <div className='text-container'>
+            <h1>Improve your front-end skills by building projects</h1>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          </div>
+      </section>
     </main>
   );
 }
